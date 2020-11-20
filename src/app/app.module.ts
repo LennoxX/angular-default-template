56c75b1b391bfeeb2da2373781fb3e54a8608484
Lucas Raphael Fernandes Ferreira <lucasraphael.fernandes@hotmail.com>
2020-11-20 15:17:35 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [NavComponent, MenuComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    NavComponent,
+    MenuComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
